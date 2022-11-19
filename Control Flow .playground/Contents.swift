eedff2bae9c12afa4bcb5e5fedd7664e091988af
백대홍 <bdh3620@naver.com>
2022-11-19 10:23:44 +0900

    조건문

--- Control Flow .playground/Contents.swift
+++ Control Flow .playground/Contents.swift
@@ -41,3 +41,67 @@ print("\(base) to the power of \(power) is \(answer)")
 //while condition {
 //    statements
 //}
+
+
+// MARK: - Repeat-While
+
+//repeat-while 루프로 알려진 while 루프의 다른 변형은 루프의 조건을 고려하기 전에 먼저 루프 블록을 통해 단일 패스를 수행합니다. 그런 다음 조건이 거짓이 될 때까지 루프를 계속 반복합니다.
+
+//일반적인 형식
+//repeat {
+//    statements
+//} while condition
+
+
+// MARK: - 조건문
+//가장 간단한 형태의 if명령문에는 단일 if조건이 있습니다.
+
+var temperatureInFahrenheit = 30
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+}
+
+
+//if-else 문
+temperatureInFahrenheit = 40
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else {
+    print("It's not that cold. Wear a t-shirt.")
+}
+
+
+temperatureInFahrenheit = 90
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+} else {
+    print("It's not that cold. Wear a t-shirt.")
+}
+
+// MARK: - 스위치
+
+
+//스위치 문은 값을 고려하여 가능한 여러 일치 패턴과 비교합니다. 그런 다음 성공적으로 일치하는 첫 번째 패턴을 기반으로 적절한 코드 블록을 실행합니다. 스위치 문은 여러 잠재적 상태에 응답하기 위한 if 문에 대한 대안을 제공합니다.
+//switch some value to consider {
+//case value 1:
+//   respond to value 1
+//case value 2,
+//     value 3:
+//    respond to value 2 or 3
+//default:
+//    otherwise, do something else
+//}
+
+let someCharacter: Character = "z"
+switch someCharacter {
+case "a":
+    print("The first letter of the alphabet")
+case "z":
+    print("The last letter of the alphabet")
+default:
+    print("Some other character")
+}
+
+//여기서 someCharacter가 a와 z둘다 아닐시 default값인 print("Some other character")를 출력하게 된다
