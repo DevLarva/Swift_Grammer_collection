d2e94b4d446440724a84a65a2d665db5b69a8802
백대홍 <bdh3620@naver.com>
2022-11-21 00:17:36 +0900

    Function

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,6 +1,34 @@
+
+// MARK: -함수 정의 및 호출
+
 func greet(person: String) -> String {
     let greeting = "Hello, " + person + "!"
     return greeting
 }
 
- 
+
+print(greet(person:"Anna"))
+print(greet(person:"Brian"))
+
+func greetAgain(person:String) -> String {
+    return "Hello again, " + person + "!"
+    
+}
+print(greetAgain(person:"Anna"))
+
+
+// MARK: -함수 매개변수 및 반환 값
+
+func sayHelloWorld() -> String {
+    return "hello, world"
+}
+print(sayHelloWorld())
+
+
+func greet(person: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+}
